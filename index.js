00fcbdc47d631e1e6d9cf1277e2f734734035c77
AsmaaMoallim <asmaaam1o1@gmail.com>
2022-01-24 07:17:23 +0300

    get and post

--- index.js
+++ index.js
@@ -1,24 +1,70 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const port = process.env.PORT || 3000;
-app.listen(port , () => {
-    console.log(`Listening to port ${port}`);
+// validiation function using joi
+function validateGenre(genre){
+const schema = Joi.object({
+   genreName : Joi.string().required(),
 })
+return schema.validate(genre)
+}
+
+// defining the genres array
+const genres = [
+  {
+    id: 1,
+    genreName: "horror",
+  },
+  {
+    id: 2,
+    genreName: "romance",
+  },
+  {
+    id: 3,
+    genreName: "drama",
+  },
+  {
+    id: 4,
+    genreName: "fiction",
+  },
+];
+
+
+// get method
+app.get("/" || "/vidly.com", (req, res) => {
+  res.send("Welcome to Vidly");
+});
+
+app.get("/vidly.com/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+app.get("/vidly.com/api/genres/:id", (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id))
+  if(!genre) return res.status(400).res.send("this genre does not exist");
+
+  res.send(genre);
+});
+
+// post method
+app.post("/vidly.com/api/genres", (req, res) => {
+
+  const {error} = validateGenre(req.body);
+  if(error) return res.status(400).send(error.message);
+
+  const genre = {
+    id: genres.length + 1,
+    genreName: req.body.genreName,
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+
+// listening to port
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
