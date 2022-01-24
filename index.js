2267362246cb9dd6caf96accc8eff2483e042c77
AsmaaMoallim <asmaaam1o1@gmail.com>
2022-01-24 07:23:39 +0300

    put and delete

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const Joi = require("joi");
 const express = require("express");
 const app = express();
 
-
 // validiation function using joi
-function validateGenre(genre){
-const schema = Joi.object({
-   genreName : Joi.string().required(),
-})
-return schema.validate(genre)
+function validateGenre(genre) {
+  const schema = Joi.object({
+    genreName: Joi.string().required(),
+  });
+  return schema.validate(genre);
 }
 
 // defining the genres array
@@ -31,7 +30,6 @@ const genres = [
   },
 ];
 
-
 // get method
 app.get("/" || "/vidly.com", (req, res) => {
   res.send("Welcome to Vidly");
@@ -42,17 +40,16 @@ app.get("/vidly.com/api/genres", (req, res) => {
 });
 
 app.get("/vidly.com/api/genres/:id", (req, res) => {
-  const genre = genres.find(g => g.id === parseInt(req.params.id))
-  if(!genre) return res.status(400).res.send("this genre does not exist");
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(400).res.send("this genre does not exist");
 
   res.send(genre);
 });
 
 // post method
 app.post("/vidly.com/api/genres", (req, res) => {
-
-  const {error} = validateGenre(req.body);
-  if(error) return res.status(400).send(error.message);
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.message);
 
   const genre = {
     id: genres.length + 1,
@@ -62,6 +59,28 @@ app.post("/vidly.com/api/genres", (req, res) => {
   res.send(genre);
 });
 
+// put method
+app.put("/vidly.com/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(400).res.send("you updating a genre that does not exist");
+
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.message);
+
+  genre.genreName = req.body.genreName;
+  res.send(genre);
+});
+
+// delete method 
+app.delete("/vidly.com/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) return res.status(400).res.send("you deleting a genre that does not exist");
+
+  const indexOfGenre = genres.indexOf(genre);
+  genres.splice(indexOfGenre,1);
+  res.send(genre);
+});
 
 // listening to port
 const port = process.env.PORT || 5000;
